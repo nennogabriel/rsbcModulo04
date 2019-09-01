589a8367cf37781f8e89134098315b53f69460be
Pedro Nascimento <nennogabriel@gmail.com>
2019-09-01 16:11:52 -0300

    04a08 refactoring an component with props

---
+++ src/components/TechItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function TechItem({ tech, onDelete}) {
+  return (
+    <li>
+    {tech}
+    <button onClick={onDelete} type="button">Remover</button>
+    </li>
+  );
+}
